037a6577dd4cc0df5e605938389ccb9d524238c2
lvu <lvu@pros.com>
2017-06-23 01:36:47 -0500

    regex complete

--- app/regex.js
+++ app/regex.js
@@ -2,26 +2,33 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    return /[0-9]/.test(str);
   },
 
   containsRepeatingLetter: function(str) {
-
+    return /([a-zA-Z])\1+/i.test(str);
   },
 
   endsWithVowel: function(str) {
-
+    return /[aeiou]$/i.test(str);
   },
 
   captureThreeNumbers: function(str) {
-
+    var match = /\d\d\d/.exec(str);
+    if (match != null) {
+      return match[0];
+    }
+    return false;
   },
 
   matchesPattern: function(str) {
-
+    return /^\d\d\d\-\d\d\d\-\d\d\d\d$/.test(str);
   },
 
   isUSD: function(str) {
+    // always start with $
+    // always end with .xx
 
+    return /^\$\d{1,3}(,\d{3})*(\.\d{2})?$/.test(str);
   }
 };
